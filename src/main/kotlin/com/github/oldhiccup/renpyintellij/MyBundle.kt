4c67e7359bae58064821128997192fb86494725e
GitHub Action <action@github.com>
2021-02-20 19:53:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/oldhiccup/renpyintellij/MyBundle.kt
+++ src/main/kotlin/com/github/oldhiccup/renpyintellij/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.oldhiccup.renpyintellij
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/oldhiccup/renpyintellij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/oldhiccup/renpyintellij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.oldhiccup.renpyintellij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.oldhiccup.renpyintellij.MyBundle
 
 class MyApplicationService {
 
